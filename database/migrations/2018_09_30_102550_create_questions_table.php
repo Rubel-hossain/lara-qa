63e3baaaaf30b6ef567431fdc15f1d2403aeeb95
Rubel-hossain <rubel8525@yahoo.com>
2018-09-30 22:05:52 +0600

    relationship between question and user model

---
+++ database/migrations/2018_09_30_102550_create_questions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('slug');
+            $table->text('body');
+            $table->UnsignedInteger('views')->default(0);
+            $table->integer('votes')->default(0);
+            $table->UnsignedInteger('answers')->default(0);
+            $table->UnsignedInteger('best_answer_id')->nullable();
+            $table->UnsignedInteger('user_id');
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
