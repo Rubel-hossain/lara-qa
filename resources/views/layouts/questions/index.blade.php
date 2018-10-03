e773e4c4f15dff63de8ee56253f189c0cf997ab4
Rubel-hossain <rubel8525@yahoo.com>
2018-10-04 01:20:43 +0600

    views questiona answers,vote,views

--- resources/views/layouts/questions/index.blade.php
+++ resources/views/layouts/questions/index.blade.php
@@ -6,16 +6,23 @@
         <div class="col-md-8">
             <div class="card">
              @foreach($questions as $question)
-              <div class="single-topics">
-                <div class="card-header">
-                    <h4><a href="{{$question->url}}">{{$question->title}}</a></h4>
-                   <p class="lead mb-0">
-                       Asked By <a href="{{$question->user->url}}">{{$question->user->name}}</a>
-                       <small class="text-muted">{{$question->created_date}}</small>
-                   </p>
-                </div>
-                <div class="card-body">
-                   <p>{{str_limit($question->body,200)}}</p>
+             <div class="single-topics d-flex">
+              <div class="topics-response">
+                <span class="votes text-center"><strong>{{$question->votes}}</strong> {{str_plural('Vote',$question->votes)}}</span>
+                <span class="views text-center">{{$question->views}} {{str_plural('View',$question->views)}}</span>
+                <span class="answers answered text-center">{{$question->answers}} {{str_plural('Answer',$question->answers)}}</span>
+              </div>
+              <div class="topics-content">
+                    <div class="card-header">
+                        <h4><a href="{{$question->url}}">{{$question->title}}</a></h4>
+                       <p class="lead mb-0">
+                           Asked By <a href="{{$question->user->url}}">{{$question->user->name}}</a>
+                           <small class="text-muted">{{$question->created_date}}</small>
+                       </p>
+                    </div>
+                    <div class="card-body">
+                       <p>{{str_limit($question->body,200)}}</p>
+                    </div>
                 </div>
               </div>
               @endforeach
